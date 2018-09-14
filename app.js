b9d1bde13131cba8cb5c85c513d42f3efae9cd3d
Blaise Clarke <blaiserclarke@gmail.com>
2018-09-14 14:11:14 -0700

    Updated app.js to complete function sumAndMultiply, passes third test in test.js

--- app.js
+++ app.js
@@ -52,11 +52,17 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-
+    let triSum = sum(a, sum(b, c)[0] )[0]; //using recursion to save an intermediate variable
+    let triMult = multiply(a, multiply(b,c)[0])[0];
+    let triSumMessage = a + ' and ' + b + ' and ' + c + ' sum to ' + triSum + '.';
+    let triMultMessage = 'The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + triMult + '.';
+    //console.log(triSumMessage);
+    //console.log(triMultMessage);
+    return [triSum, triMult, triSumMessage, triMultMessage];
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
